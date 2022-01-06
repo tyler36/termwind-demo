fe81e2023b9c4dc4da1c8f9ec9e722eb4308b4c2
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-01-06 09:35:44 +0900

    refactor to TermWind

--- r-laravel.php
+++ r-laravel.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Zttp\Zttp;
+use function Termwind\{render};
 
 $response = Zttp::get('https://reddit.com/r/laravel.json')->json();
 
@@ -13,10 +14,19 @@ if (array_key_exists('error', $response)) {
 
 foreach ($response['data']['children'] as $thread) {
     $title = $thread['data']['title'];
+    $url = $thread['data']['url'];
     $author = $thread['data']['author'];
     $upvotes = $thread['data']['ups'];
 
-    echo $title . PHP_EOL;
-    echo "Author: {$author} | Upvotes: {$upvotes}"  . PHP_EOL;
-    echo "" . PHP_EOL;
+    // echo $title . PHP_EOL;
+    // echo "Author: {$author} | Upvotes: {$upvotes}"  . PHP_EOL;
+    // echo "" . PHP_EOL;
+
+    $renderHtml = "<div class='mb-2'>
+        <a href='{$url}' class='font-bold underline'>{$title}</a><br>
+        <span class='bg-blue-700 text-blue-200 p-2'>{$author}</span>
+        <span class='bg-green-700 text-green-200 p-2 ml-2'>{$upvotes}</span>
+    </div>";
+
+    render($renderHtml);
 }
--- readme.md
+++ readme.md
@@ -2,11 +2,13 @@
 
 - [Description](#description)
 - [Demo](#demo)
+- [Adding TermWind](#adding-termwind)
 
 ## Description
 
 - "Termwind allows you to build unique and beautiful PHP command-line applications, using the Tailwind CSS API. In short, it's like Tailwind CSS, but for the PHP command-line applications."
 - [Homepage](https://github.com/nunomaduro/termwind)
+- Requires `PHP 8.0+`
 
 ## Demo
 
@@ -20,4 +22,27 @@ $ php r-laravel.php
 ...
 Deploy project on Ubuntu Server
 Author: frevelmann | Upvotes: 8
+```
+
+## Adding TermWind
+
+- Add Termwind
+
+```shell
+composer require nunomaduro/termwind
+```
+
+- Import the function
+
+```php
+
+use function Termwind\{render};
+```
+
+- Write required HTML with tailwind classes and render to display.
+
+```php
+$renderHtml = "<div class='font-bold'>Example Heading</div>";
+
+render($renderHtml);
 ```
